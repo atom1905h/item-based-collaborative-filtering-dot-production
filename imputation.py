36d2e93ccd36d72cb45c5434cd35528707f782fa
atom <atom1905h@naver.com>
2023-12-01 11:20:56 +0900

    [feat] imputation

---
+++ imputation.py
@@ -0,0 +1,54 @@
+from sklearn.impute import KNNImputer
+
+def fill_zero(matrix):
+    matrix = matrix.fillna(0)
+
+    return matrix
+
+def fill_mean_1(matrix):
+    for col in matrix.index:
+        value = matrix.loc[col].mean()
+        matrix.loc[col] = matrix.loc[col].fillna(value)
+
+    return matrix
+
+def fill_mean_2(matrix):
+    for col in matrix.columns:
+        value = matrix[col].mean()
+        matrix[col] = matrix[col].fillna(value)
+
+    return matrix
+
+def fill_median_1(matrix):
+    for col in matrix.index:
+        value = matrix.loc[col].meadian()
+        matrix.loc[col] = matrix.loc[col].fillna(value)
+
+    return matrix
+
+def fill_median_2(matrix):
+    for col in matrix.columns:
+        value = matrix[col].meadian()
+        matrix[col] = matrix[col].fillna(value)
+
+    return matrix
+
+def fill_mode_1(matrix):
+    for col in matrix.index:
+        value = matrix.loc[col].mode()
+        matrix.loc[col] = matrix.loc[col].fillna(value)
+
+    return matrix
+
+def fill_mode_2(matrix):
+    for col in matrix.columns:
+        value = matrix[col].mode()
+        matrix[col] = matrix[col].fillna(value)
+
+    return matrix
+
+def fill_knn(matrix, n):
+    knn_imputer = KNNImputer(n_neighbors=n)
+    user_item_matrix_imputed = knn_imputer.fit_transform(matrix)
+
+    return user_item_matrix_imputed
