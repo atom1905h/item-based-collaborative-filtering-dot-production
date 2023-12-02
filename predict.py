6c37e66e7d3579d63d359cd44a2c37bf93fa8915
atom <atom1905h@naver.com>
2023-12-02 12:25:21 +0900

    [feat] add predict rating

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def predict_user_ratings(user_id, user_ratings, item_similarity):
+def predict_user_unrated_ratings(user_id, user_ratings, item_similarity):
     pred_ratings=[]
     unrated_movies = user_ratings[user_ratings[user_id].isnull()][user_id].index.tolist()
     rated_movies =user_ratings[user_ratings[user_id].notnull()][user_id].index.tolist()
@@ -10,6 +10,20 @@ def predict_user_ratings(user_id, user_ratings, item_similarity):
     for i in unrated_movies:
         pred_rating = np.matmul(item_similarity[i][rated_movies],watched_user_y)/(sum(item_similarity[i][rated_movies])+1)
         pred_ratings.append(pred_rating[0])
-    result = pd.DataFrame({'movieId': unrated_movies, 'predicted_rating': pred_ratings})
+    result = pd.DataFrame({'movieId': unrated_movies, 'userId':user_id, 'predicted_rating': pred_ratings})
+
+    return result
+
+def predict_user_rated_ratings(user_id,user_ratings, item_similarity):
+    pred_ratings = []
+    rated_movies =user_ratings[user_ratings[user_id].notnull()][user_id].index.tolist()
+    
+    for i in rated_movies:
+        rated_movies_except_i = [movie for movie in rated_movies if movie != i]
+        user_rating = user_ratings[user_id].loc[rated_movies_except_i].tolist()
+        watched_user_y = np.array(user_rating).reshape(-1, 1)
+        pred_rating = np.matmul(item_similarity[i][rated_movies_except_i], watched_user_y)/(sum(item_similarity[i][rated_movies_except_i]) + 1)
+        pred_ratings.append(pred_rating[0])
+    result = pd.DataFrame({'movieId': rated_movies, 'userId':user_id, 'predicted_rating': pred_ratings})
 
     return result
