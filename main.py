270246b678d9baf30c8dc034762b018a208b61b5
atom <atom1905h@naver.com>
2023-12-04 14:53:42 +0900

    [feat] mds, psd, jmsd, jpss similarity

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from argparse import ArgumentParser
 from imputation import fill_zero, fill_mean_1, fill_mean_2, fill_knn, fill_median_1, fill_median_2, fill_mode_1, fill_mode_2
-from similarity import to_dataframe, cosine_sim, euclidean_sim, jaccard_sim, pearson_sim, manhattan_sim
-from vectorization import item_rating, item_difference_release_rating, item_rating_average_deviation, item_rating_or_not, item_rating_tag_or_not, item_tag_or_not
-from predict import predict_user_ratings
+from similarity import to_dataframe, cosine_sim, euclidean_sim, jaccard_sim, pearson_sim, manhattan_sim, msd_similarity_matrix_parallel, pss
+from vectorization import item_rating, item_difference_release_rating, item_rating_average_deviation, item_rating_or_not, item_rating_tag_or_not, item_tag_or_not, item_tags_count, item_tf_idf, item_tf_idf_sum
+from predict import predict_user_unrated_ratings, predict_user_rated_ratings
 import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
 
 def parse_args():
     parser = ArgumentParser()
@@ -38,42 +40,99 @@ def recommend(vectorization, imputation, similarity, user_id):
         vector = item_rating_tag_or_not(df)
     elif vectorization=='item_tag_or_not':
         vector = item_tag_or_not(df)
+    elif vectorization=='item_tags_count':
+        vector = item_tags_count(df)
+    elif vectorization=='item_tf_idf_sum':
+        vector = item_tf_idf_sum(df)
+    elif vectorization=='item_tf_idf':
+        vector = item_tf_idf(df)
 
     if imputation=='fill_zero':
-        vector = fill_zero(vector)
+        vector1 = fill_zero(vector)
     elif imputation=='fill_mean_1':
-        vector = fill_mean_1(vector)
+        vector1 = fill_mean_1(vector)
     elif imputation=='fill_mean_2':
-        vector = fill_mean_2(vector)
+        vector1 = fill_mean_2(vector)
     elif imputation=='fill_knn':
-        vector = fill_knn(vector)
+        vector1 = fill_knn(vector,3)
     elif imputation=='fill_median_1':
-        vector = fill_median_1(vector)
+        vector1 = fill_median_1(vector)
     elif imputation=='fill_median_2':
-        vector = fill_median_2(vector)
+        vector1 = fill_median_2(vector)
     elif imputation=='fill_mode_1':
-        vector = fill_mode_1(vector)
+        vector1 = fill_mode_1(vector)
     elif imputation=='fill_mode_2':
-        vector = fill_mode_2(vector)
+        vector1 = fill_mode_2(vector)
+    elif imputation=='no':
+        vector1 = vector
 
     if similarity=='cosine':
-         matrix = cosine_sim(vector)
-    if similarity=='euclidean':
-         matrix = euclidean_sim(vector)
-    if similarity=='jaccard':
-         matrix = jaccard_sim(vector)
-    if similarity=='pearson':
-         matrix = pearson_sim(vector)
-    if similarity=='manhattan':
-         matrix = manhattan_sim(vector)
-    
-    similarity_matrix = to_dataframe(matrix, vector)
-    predicted_ratings = predict_user_ratings(user_id, user_ratings=user_ratings, item_similarity=similarity_matrix)
-    predicted_ratings_sorted = predicted_ratings.sort_values(by='predicted_rating', ascending=False)
-    top_10=predicted_ratings_sorted.head(10)['movieId'].tolist()
-    top_10_movies = movies[movies['movieId'].isin(top_10)]
+         matrix = cosine_sim(vector1)
+    elif similarity=='euclidean':
+         matrix = euclidean_sim(vector1)
+    elif similarity=='jaccard':
+         matrix = jaccard_sim(vector1)
+    elif similarity=='pearson':
+         matrix = pearson_sim(vector1)
+    elif similarity=='manhattan':
+         matrix = manhattan_sim(vector1)
+    elif similarity=='msd':
+         matrix = msd_similarity_matrix_parallel(vector1)
+    elif similarity=='pss':
+         matrix = pss(vector1)
+    elif similarity=='jmsd':
+        matrix_j = jaccard_sim(vector1)
+        matrix_m = msd_similarity_matrix_parallel(vector1)
+        matrix = matrix_j*matrix_m
+    elif similarity=='jpss':
+        matrix_j = jaccard_sim(vector1)
+        matrix_p = pss(vector1)
+        matrix = matrix_j*matrix_p
 
-    print(top_10_movies['title'])
+    if vectorization=='item_tf_idf':
+        similarity_matrix = pd.DataFrame(matrix,
+                                index=movies['movieId'].unique().tolist(), columns=movies['movieId'].unique().tolist())
+    else:
+        similarity_matrix = to_dataframe(matrix, vector)
+
+    user_list = df['userId'].unique().tolist()
+    result = predict_user_rated_ratings(user_list[0], user_ratings=user_ratings, item_similarity=similarity_matrix)
+    for i in user_list[1:]:
+        predicted_ratings = predict_user_rated_ratings(i, user_ratings=user_ratings, item_similarity=similarity_matrix)
+        result= pd.concat([result, predicted_ratings],axis=0)
+
+    result2=pd.merge(df, result, on=['userId','movieId'],how='left')
+    result2_not_null = result2.dropna(subset=['rating'])
+    true_y = np.array(result2_not_null['rating'])
+    pred_y = np.array(result2_not_null['predicted_rating'])
+    mae = mean_absolute_error(y_true=true_y, y_pred=pred_y)
+    mse = mean_squared_error(y_true=true_y, y_pred=pred_y)
+    rmse = np.sqrt(mse)
+    name = vectorization+"_"+imputation+"_"+ similarity
+
+    result_metric = pd.read_csv('result/result.csv')
+    new_result_metric = pd.DataFrame(data=[[name, mae, mse, rmse]], columns=['name','mae','mse','rmse'])
+    result_metric = result_metric.append(new_result_metric)
+    result_metric.to_csv('result/result.csv', index=False)
+
+
+    # recommended_movies_df = pd.DataFrame(columns=['userId', 'movieId', 'title','genres','year'])
+    # for j in user_list:
+    #     predicted_unrated_ratings = predict_user_unrated_ratings(j, user_ratings=user_ratings, item_similarity=similarity_matrix)
+    #     predicted_ratings_sorted = predicted_unrated_ratings.sort_values(by='predicted_rating', ascending=False)
+    #     top_5=predicted_ratings_sorted.head(5)['movieId'].tolist()
+    #     top_5_movies = movies[movies['movieId'].isin(top_5)][['movieId', 'title','genres','year']]
+    #     top_5_movies['userId'] = j
+    #     recommended_movies_df = recommended_movies_df.append(top_5_movies)
+
+    # recommended_movies_df.to_csv(f'result/{name}.csv',index=False)
+
+    # predicted_unrated_ratings = predict_user_unrated_ratings(user_id, user_ratings=user_ratings, item_similarity=similarity_matrix)
+    # predicted_ratings_sorted = predicted_unrated_ratings.sort_values(by='predicted_rating', ascending=False)
+    # top_10=predicted_ratings_sorted.head(10)['movieId'].tolist()
+    # top_10_movies = movies[movies['movieId'].isin(top_10)]
+
+    # print(top_10_movies['title'])
 
 def main(args):
     recommend(**args.__dict__)
